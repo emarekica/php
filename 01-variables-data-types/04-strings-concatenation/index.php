02849d40f928d8ebe16c6a7e43dbc0879ece4540
emarekica <emarekica@gmail.com>
2026-01-04 23:13:55 +0100

    Add examples of string concatenation and variable interpolation

--- 01-variables-data-types/04-strings-concatenation/index.php
+++ 01-variables-data-types/04-strings-concatenation/index.php
@@ -1,3 +1,15 @@
+<?php
+  $firstName = 'Ann';
+  $lastName = 'Doe';
+  $anotherPerson = 'John Doe';
+
+  // string concatenation
+  $fullName0 = $firstName . $lastName;
+
+  // with space
+  $fullName = $firstName . ' ' . $lastName;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,6 +30,15 @@
     <div class="bg-white rounded-lg shadow-md p-6">
       <!-- Output -->
 
+      <!-- inserting string into another string (literal strings) -->
+      <?php echo 'Hello, my name is ' . $fullName . '<br>' ?>
+
+      <!-- Escaping characters -->
+      <?php echo "Hello, my name is 'John'"?><br>
+      <?php echo 'Hello, my name is \'John\''?><br>
+
+      <!-- VARIABLE INTERPOLATION with double-quotes -->
+       <?= "Hello, my name is $anotherPerson" ?>
     </div>
   </div>
 </body>
