8146b8affa8a34d86f625da0cbb32677772afa58
emarekica <emarekica@gmail.com>
2026-01-13 22:54:12 +0100

    Add Multidimensional array example

--- 02-arrays-and-iteration/04-multi-dimensional-arrays/index.php
+++ 02-arrays-and-iteration/04-multi-dimensional-arrays/index.php
@@ -1,5 +1,47 @@
 <?php
+// data fetched from a database is often stored as multi-dimensional array
 $output = null;
+
+$fruits = [
+  ["Apple", "red"],
+  ["Orange", "orange"],
+  ["Banana", "yellow"],
+];
+
+// Accessing elements
+$output = $fruits[0][0]; // apple
+$output = $fruits[0][1]; // red
+
+// Add a nested array
+$fruits[] = ["Pear", "green"];
+
+// array of nested associative arrays - more realistic
+$users = [
+  ['name' => 'John', 'email' => 'john@gmail.com', 'fauxPass' => 'secret'],
+  ['name' => 'Mary', 'email' => 'mary@gmail.com', 'fauxPass' => 'secret'],
+  ['name' => 'Jane', 'email' => 'jane@gmail.com', 'fauxPass' => 'secret']
+];
+
+// Access Mary's email
+$output = $users[1]["email"];
+
+// Add a user
+$users[] = ['name' => 'Mike', 'email' => 'mike@gmail.com', 'fauxPass' => 'secret'];
+array_push($users, ['name' => 'Julia', 'email' => 'julia@gmail.com', 'fauxPass' => 'secret']);
+
+// Remove the first user
+array_shift($users);
+
+// Remove the last user
+array_pop($users);
+
+// Remove a specific user
+unset($users[1]);
+
+// Count the users
+
+$output = count($users); // 2
+
 ?>
 
 <!DOCTYPE html>
@@ -20,8 +62,12 @@ $output = null;
   </header>
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
-      <!-- Output -->
       <p class="text-xl"><?= $output ?></p>
+      <p>
+        <pre>
+          <?php print_r($users); ?>
+        </pre>
+      </p>
     </div>
   </div>
 </body>
