1a88a6b4889176458ad3b1bd148ec40ff574a90e
emarekica <emarekica@gmail.com>
2026-01-16 17:48:05 +0100

    Finish Arrays challenge

--- 02-arrays-and-iteration/05-array-challenges/index.php
+++ 02-arrays-and-iteration/05-array-challenges/index.php
@@ -65,10 +65,58 @@ print_r($colors); // ['yellow', 'pink', 'blue', 'red', 'purple']
 /*
   Challenge 3: Job listings array
 
-  1. Create a multi-dimensional array of associative arrays of 3 job listings with the fields id, job_title, company, contact_email, and contact_phone. Also add an array field for skills. The skills array should be an array of strings with each skill a person has. For example, 'PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS', etc.
+  1. Create a multi-dimensional array of associative arrays of 3 job listings with the fields id, job_title, company, contact_email, and contact_phone.
+  Also add an array field for skills. The skills array should be an array of strings with each skill a person has. For example, 'PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS', etc.
   2. Create a new listing using the `array_push()` function. The new listing should have the same fields as the others.
   3. Print out the job_title of the second job listing in the array.
   4. Print out the first skill of the third job listing in the array.
 */
 
 echo '<h3>Job Listings</h3>';
+
+// 1.
+$job_listings = [
+    [
+    'id' => 1,
+    'job_title' => 'PHP Developer',
+    'company' => 'ABC Company',
+    'contact_email' => 'john@email.com',
+    'contact_phone' => '123-456-7890',
+    'skills' => ['PHP', 'MySQL', 'JavaScript', 'HTML', 'CSS']
+  ],
+  [
+    'id' => 2,
+    'job_title' => 'Web Designer',
+    'company' => 'XYZ Company',
+    'contact_email' => 'steph@email.com',
+    'contact_phone' => '123-456-7890',
+    'skills' => ['Photoshop', 'Illustrator', 'HTML', 'CSS']
+  ],
+  [
+    'id' => 3,
+    'job_title' => 'Web Developer',
+    'company' => '123 Company',
+    'contact_email' => 'peter@email.com',
+    'contact_phone' => '123-456-7890',
+    'skills' => ['Python', 'PostgreSQL', 'JavaScript', 'HTML', 'CSS']
+  ]
+];
+
+// 2.
+array_push($job_listings, [
+  'id' => 4,
+  'job_title' => 'C# Developer',
+  'company' => '123 Company',
+  'contact_email' => 'kristen@email.com',
+  'contact_phone' => '123-456-7890',
+  'skills' => ['C#', 'Angular', 'JavaScript', 'HTML', 'CSS']
+]);
+
+// 3.
+// $secondTitle = $job_listings[1]['job_title'];
+// print_r($secondTitle);
+
+echo $job_listings[1]['job_title'];
+
+// 4.
+echo $job_listings[2]['skills'][0];
